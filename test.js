941e8942c62f1b7929686e6593656646a697c188
Seth Archer Brown <learc83@gmail.com>
2022-11-30 20:14:26 -0500

    Add ident testing

--- test.js
+++ test.js
@@ -10,22 +10,43 @@ function assertEq(actual, expected) {
 
 function assertLex(lex, expected_tokens, expected_errors) {
   if (lex.tokens.length != expected_tokens.length) {
+    // TODO compress these
     console.log(`Failed--actual token length doesn't equal expected--${lex.tokens.length} vs. ${expected_tokens.length}`);
+    console.log('actual');
+    console.log(lex.tokens);
+    console.log('expected');
+    console.log(expected_tokens);
+    console.log('------------');
     return
   }
   if (lex.errors.length != expected_errors.length) {
     console.log(`Failed--actual errors length doesn't equal expected--${lex.errors.length} vs. ${expected_errors.length}`);
+    console.log('actual');
+    console.log(lex.errors);
+    console.log('expected');
+    console.log(expected_errors);
+    console.log('------------');
     return
   }
   for (let i = 0; i < lex.tokens.length; i++) {
     const actual = lex.tokens[i];
     const expected = expected_tokens[i]
     if (actual.type != expected.type && actual.lexeme != expected.lexeme) {
-      console.log(`Failed--actual token doesn't equal expected--${actual} vs. ${expected}`);
+      console.log(`Failed--actual token doesn't equal expected`);
+      console.log('actual');
+      console.log(lex.tokens);
+      console.log('expected');
+      console.log(expected_tokens);
+      console.log('------------');
       return
     }
     if (lex.errors[i] != expected_errors[i]) {
       console.log(`Failed--actual error doesn't equal expected--${lex.errors[i]} vs. ${expected_errors[i]}`);
+      console.log('actual');
+      console.log(lex.errors);
+      console.log('expected');
+      console.log(expected_errors);
+      console.log('------------');
       return
     }
   }
@@ -45,6 +66,10 @@ function random_char(arr) {
   return random_pick;
 }
 
+// TODO What should EOF have as it's value empty quotes, nul, "EOF?"
+const EOF = new Token(TokenType.eof, "");
+
+// atom 
 function atom_start() {
   return random_char(['_', ['a', 'z'], ['A', 'Z']]);
 }
@@ -54,7 +79,6 @@ function atom_middle() {
   let str = "";
   for (let i = 0; i < times; i++) {
     str += random_char(['', '_', '@', ['a', 'z'], ['A', 'Z'], ['0', '9']]);
-
   }
   return str;
 }
@@ -64,14 +88,40 @@ function atom_end() {
 }
 
 for (let i = 0; i < 1000; i++) {
-  const template = `:${atom_start() + atom_middle() + atom_end()} `
-  lex = new Lexer(template);
+  const template = `${atom_start() + atom_middle() + atom_end()} `
+  const string = ":" + template
+  lex = new Lexer(string);
   lex.scanTokens();
-  assertEq(lex.errors.length, 0);
+  expected_tokens = [new Token(TokenType.atom, template), EOF];
+  assertLex(lex, expected_tokens, []);
 }
 
-/// @next
-// Maybe add property testing with a template for atoms
-// Have some kind of function where you can pass in the src, tokens, and errors and it asserts they match
-// and then write a loop that can build a list of those structures (src, tokens, errors)
-// also have asserts log the lexer
+// identifier
+function ident_start() {
+  return random_char(['_', ['a', 'z']]);
+}
+
+function ident_middle() {
+  const times = Math.floor(Math.random() * 3)
+  let str = "";
+  for (let i = 0; i < times; i++) {
+    str += random_char(['', '_', ['a', 'z'], ['A', 'Z'], ['0', '9']]);
+  }
+  return str;
+}
+
+function ident_end() {
+  return random_char(['', '!', '?']);
+}
+
+for (let i = 0; i < 100000; i++) {
+  const string = `${ident_start() + ident_middle() + ident_end()} `
+  lex = new Lexer(string);
+  lex.scanTokens();
+  expected_tokens = [new Token(TokenType.ident, string), EOF];
+  assertLex(lex, expected_tokens, []);
+}
+
+// @next
+// fix the errors with in the testing when keywords are accidentally generated
+// compress the stuff in the TODO above
